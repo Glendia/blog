529a67a028e59af5f2553ec7a1ee6d55db93158d
Glendia <49245772+Glendia@users.noreply.github.com>
2024-06-11 17:49:49 +0800

    config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
   titleTemplate: ':title - Developer Blog | 编织代码梦想，启航开发者未来',
   description: "探索编程新境界，连接开发者智慧。我们提供前沿技术资讯、实战教程、高效工具与深度交流平台，激发创意，助力每一位开发者成就不凡。从Vue.js到VitePress，携手构建互联网的明天",
   head: head,
-  outDir: './docs',
+  outDir: './dist',
   assetsDir: './assets',
   cacheDir: './.vitepress/.vite',
   themeConfig: {
